c471afbf8383eb5317d3faee5e28c14a613bfffc
Joseph Wang <joseph4x4@gmail.com>
2024-07-30 01:32:48 -0400

    chore: add space to duration format

--- formatters.py
+++ formatters.py
@@ -2,4 +2,4 @@ from constants import MINUTE
 
 
 def format_duration(time_in_seconds):
-    return f"{time_in_seconds//MINUTE:02}m{time_in_seconds%MINUTE:02}s"
+    return f"{time_in_seconds//MINUTE:02}m {time_in_seconds%MINUTE:02}s"
